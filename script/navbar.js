11f1d97d45610a3185b18bb113d397cf2383824a
zonei <haom242@gmail.com>
2024-11-04 00:01:18 +0700

    buoi toi chill

--- script/navbar.js
+++ script/navbar.js
@@ -1,20 +1,23 @@
 
 // Khai báo các đối tượng cần thiết
-
 var Menu_viewpage = document.querySelector("#Menu .viewpage");
 var Menu_merchandise = document.querySelector("#Menu .merchandise");
 var Menu_revenue = document.querySelector("#Menu .revenue");
 var Menu_userlist = document.querySelector("#Menu .userlist");
 var Menu_adminlist = document.querySelector("#Menu .adminlist");
+var Menu_order = document.querySelector("#Menu .order");
 var Display_viewpage = document.querySelector("#Display .viewpage");
 var Display_merchandise = document.querySelector("#Display .merchandise");
 var Display_revenue = document.querySelector("#Display .revenue");
 var Display_userlist = document.querySelector("#Display .userlist");
+var Display_order = document.querySelector("#Display .order");
+
 
 Display_viewpage.style.display = "none";
-Display_merchandise.style.display = "block";
+Display_merchandise.style.display = "none";
 Display_revenue.style.display = "none";
 Display_userlist.style.display = "none";
+Display_order.style.display = "none";
 
 
 
@@ -25,6 +28,7 @@ Menu_viewpage.onclick = ()=>{
     Display_merchandise.style.display = "none";
     Display_revenue.style.display = "none";
     Display_userlist.style.display = "none";
+    Display_order.style.display = "none";
 }
 
 Menu_merchandise.onclick = ()=>{
@@ -32,6 +36,7 @@ Menu_merchandise.onclick = ()=>{
     Display_merchandise.style.display = "block";
     Display_revenue.style.display = "none";
     Display_userlist.style.display = "none";
+    Display_order.style.display = "none";
 }
 
 Menu_revenue.onclick = ()=>{
@@ -39,6 +44,7 @@ Menu_revenue.onclick = ()=>{
     Display_merchandise.style.display = "none";
     Display_revenue.style.display = "block";
     Display_userlist.style.display = "none";
+    Display_order.style.display = "none";
 }
 
 Menu_userlist.onclick = ()=>{
@@ -46,4 +52,13 @@ Menu_userlist.onclick = ()=>{
     Display_merchandise.style.display = "none";
     Display_revenue.style.display = "none";
     Display_userlist.style.display = "block";
+    Display_order.style.display = "none";
+}
+
+Menu_order.onclick = ()=>{
+    Display_viewpage.style.display = "none";
+    Display_merchandise.style.display = "none";
+    Display_revenue.style.display = "none";
+    Display_userlist.style.display = "none";
+    Display_order.style.display = "block";
 }
