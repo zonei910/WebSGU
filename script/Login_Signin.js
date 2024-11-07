568fcc10b1aef7b232afa428d8c56fdde8e8b214
zonei <haom242@gmail.com>
2024-11-07 22:45:48 +0700

    sot

--- script/Login_Signin.js
+++ script/Login_Signin.js
@@ -4,7 +4,7 @@ var email = document.querySelector("#dang-nhap #email");
 var password = document.querySelector("#dang-nhap #password");
 var thiswindow = window;
 if(email.value == "hihi@gmail.com" && password.value == "hehe"){
-     thiswindow = window.open("/admin.html","AdminPage");
+     thiswindow = window.open("admin.html","AdminPage");
 }
     window.close(thiswindow);
 }
