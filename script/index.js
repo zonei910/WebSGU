24b5432e393a3b05b3d52cc0aad49eb090d22ed2
zonei <haom242@gmail.com>
2024-11-08 15:01:00 +0700

    them 1 so thu

--- script/index.js
+++ script/index.js
@@ -20,17 +20,14 @@ function home() {
     });
 }
 
+
 function listMenu(abc) {
     const elementsToHide = ["carousel","sp-hot", "sp-watch", "sp-ring", "sp-earrings", "sp-bracelet", "sp-necklace"];
-    
+
     elementsToHide.forEach(id => {
         document.getElementById(id).style.display = "none";
     });
-
-
     const bien = abc.id;
-
-
     switch (bien) {
         // case "carousel":
         //     document.getElementById("carousel").style.display = "block";
@@ -79,3 +76,52 @@ function nextSlide() {
 
 setInterval(nextSlide, 3000); 
 showSlide(currentIndex); 
+
+// Slider
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+next.onclick = function() {
+    if ( (active + 1) > lengthItems )
+        active = 0;
+    else 
+        active += 1;
+
+    reloadSlider();
+}
+
+prev.onclick = function() {
+    if ( (active - 1) < 0 )
+        active = lengthItems;
+    else    
+        active -= 1;
+
+    reloadSlider();
+}
+
+let refreshSlider = setInterval(() => {next.click()}, 7000);
+
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(() => {next.click()}, 7000);
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function(){
+        active = key;
+        reloadSlider();
+    })
+});
+// Ket thuc slider
