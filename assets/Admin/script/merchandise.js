ebda564df2aefc85cfd009deaf38ba9d21138d68
zonei <haom242@gmail.com>
2024-11-09 18:53:52 +0700

    chuan bi js them san pham

--- assets/Admin/script/merchandise.js
+++ assets/Admin/script/merchandise.js
@@ -5,25 +5,27 @@ Merchandise_additem.style.display = "none";
 
 
 Merchandise_addbtn.addEventListener("click" ,()=>{
-    var co = 0;
     Merchandise_additem.style.display = "block";
     var offbtn = document.querySelector("#Display .merchandise .add-form .head i");
     var savebtn = document.querySelector("#Display .merchandise .add-form .btn-save div");
     var upload = document.querySelector("#Display .merchandise .add-form .upload input");
     var name = document.querySelector("#Display .merchandise .add-form .name input");
-    var price = document.querySelector("#Display .merchandise .add-form .quanity input");
+    var price = document.querySelector("#Display .merchandise .add-form .price input");
+    var quantify = document.querySelector("#Display .merchandise .add-form .quanity input");
+    var description =  document.querySelector("#Display .merchandise .add-form .description textarea");
     var category = document.querySelector("#Display .merchandise .add-form .category select");
     offbtn.onclick = ()=>{
-        co = 1;
         Merchandise_additem.style.display = "none";
+        upload.value = "";
+        name.value = "";
+        price.value = "";
+        quantify = "";
+        description = "";
+        category.value = "necklace";
     }
+    
+    
 
-
-
-
-    if(co == 1) return;
-
-       
 
 
 
