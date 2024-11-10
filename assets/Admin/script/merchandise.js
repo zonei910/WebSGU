021cce29297180ae43cfa134631b94caf3fc3be2
zonei <haom242@gmail.com>
2024-11-10 17:20:36 +0700

    hihi

--- assets/Admin/script/merchandise.js
+++ assets/Admin/script/merchandise.js
@@ -3,30 +3,74 @@ var Merchandise_additem = document.querySelector("#Display .merchandise .add");
 Merchandise_additem.style.display = "none";
 
 
-
 Merchandise_addbtn.addEventListener("click" ,()=>{
     Merchandise_additem.style.display = "block";
     var offbtn = document.querySelector("#Display .merchandise .add-form .head i");
-    var savebtn = document.querySelector("#Display .merchandise .add-form .btn-save div");
+    var addbtn = document.querySelector("#Display .merchandise .add-form .btn-add");
+    var resetbtn = document.querySelector("#Display .merchandise .add-form .btn-reset");
     var upload = document.querySelector("#Display .merchandise .add-form .upload input");
     var name = document.querySelector("#Display .merchandise .add-form .name input");
     var price = document.querySelector("#Display .merchandise .add-form .price input");
     var quantify = document.querySelector("#Display .merchandise .add-form .quanity input");
     var description =  document.querySelector("#Display .merchandise .add-form .description textarea");
     var category = document.querySelector("#Display .merchandise .add-form .category select");
+
     offbtn.onclick = ()=>{
         Merchandise_additem.style.display = "none";
         upload.value = "";
         name.value = "";
         price.value = "";
-        quantify = "";
-        description = "";
-        category.value = "necklace";
+        quantify.value = "";
+        description.value = "";
+        category.value = "none";
     }
     
-    
+    resetbtn.onclick = ()=>{
+        upload.value = "";
+        name.value = "";
+        price.value = "";
+        quantify.value = "";
+        description.value = "";
+        category.value = "none";
+    }
+
+    addbtn.onclick = ()=>{
+        if(upload.value == ""){
+            alert("Vui lòng thêm ảnh");
+            upload.focus();
+        }
+        if(name.value == ""){
+            alert("Vui lòng thêm tên");
+            name.focus();
+        }
+        if(price.value == ""){
+            alert("Vui lòng thêm giá tiền");
+            price.focus();
+    }else{
+        var checkprice = parseInt(price.value);
+        if(checkprice != price.value){
+            alert("Vui lòng nhập giá tiền là số VD: 150.000 thì nhập 150000");
+            price.focus();
+        }
+    }
+        if(category.value == "none"){
+            alert("Vui lòng chọn loại hàng");
+            category.focus();
+        }
+
+        if(quantify.value == ""){
+            alert("Vui lòng thêm số lượng hiện có");
+            quantify.focus();
+        }
+
+
+        /*Tìm kiếm trong đám mảng sản phẩm rồi ném ra */
 
 
 
 
+
+
+
+}
 });
---
+++ customer.js
@@ -0,0 +1,47 @@
+const Customer = [
+    {
+        email: "nguyenhien@gmail.com",
+        password: "12345678",
+        name: "Nguyễn Hiền",
+        address: "123 Đường Lê Lợi, Quận 1, TP. HCM",
+        phone: "0987654321",
+        sex: "nu",
+    }
+
+    {
+        email: "phamminh@gmail.com",
+        password: "minhpham123",
+        name: "Phạm Minh",
+        address: "101 Đường Nguyễn Trãi, Quận 4, TP. HCM",
+        phone: "0934567890",
+        sex: "nam",
+    }
+
+    {
+        email: "hoangmai@gmail.com",
+        password: "hoangmai123",
+        name: "Hoàng Mai",
+        address: "102 Đường Lý Thường Kiệt, Quận 10, TP. HCM",
+        phone: "0976543210",
+        sex: "nu",
+    }
+
+    {
+        email: "tranthao456@gmail.com",
+        password: "password567",
+        name: "Trần Thảo",
+        address: "456 Đường CMT8, Quận 3, TP. HCM",
+        phone: "0912345678",
+        sex: "nu",
+    }
+
+    { email: "lehoang789@gmail.com",
+        password: "lehoang123",
+        name: "Lê Hoàng",
+        address: "789 Đường Võ Thị Sáu, Quận 5, TP. HCM",
+        phone: "0901234567",
+        sex: "nam",
+    }
+];
+
+
