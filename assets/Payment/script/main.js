80d8da5653c5fedc787ed28245676f98556fe688
zonei <haom242@gmail.com>
2024-11-21 22:33:52 +0700

    xong cai khung payment gio javascript nó

---
+++ assets/Payment/script/main.js
@@ -0,0 +1,24 @@
+
+let a =[1,2,3,4];
+let display = document.querySelector("#Display");
+let form = document.querySelector("#Form");
+display.style.display = "none";
+
+
+function showSanPham(){
+    form.style.display = "none"; 
+    display.style.display = "block";
+
+
+
+
+
+
+}
+
+
+
+function showThanhToan(){
+    form.style.display  = "block";
+    display.style.display = "none";
+}
