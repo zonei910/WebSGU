cd8729a3f896a9ff5d26194a47ac2e542125fc63
ChimUng <duynguyen19087@gmail.com>
2024-11-23 19:51:06 +0700

    update products

--- product.js
+++ product.js
@@ -13,184 +13,182 @@
 const Product = [
     {
         id: 1,
-        name: "White Band Ring",
-        category: "ring",
-        price:  1300000,
+        name: "Đồng Hồ Vàng Sang Trọng",
+        category: "watch",
+        price: 47500000,
         description: `
-            <span>Metal: 14K Gold</span><br>
-            <span>Metal Color: Rose</span><br>
-            <span>Stones: Round Diamond 0.95</span><br>
-            <span>18" Chain Included. Pendant Length: 1.25"</span>
+            <span>Chất liệu: Vàng 18K</span><br>
+            <span>Chuyển động: Tự động</span><br>
+            <span>Có hả năng chống nước</span><br>
+            <span>Chức năng: Ngày, Chức năng bấm giờ</span>
         `,
         images: [
-            "assets/User/img/ring/1.png",
-            "assets/User/img/ring/2.png",
-            "assets/User/img/ring/3.png",
-            "assets/User/img/ring/4.png"
+            "assets/User/img/watch/1-1.jpg",
+            "assets/User/img/watch/1-2.jpg",
+            "assets/User/img/watch/1-3.jpg",
+            "assets/User/img/watch/1-4.jpg"
         ]
     },
     {
         id: 2,
-        name: "Đồng hồ(Hàng tặng)",
+        name: "Đồng Hồ Đính Kim Cương",
         category: "watch",
-        price: 0,
+        price: 71250000,
         description: `
-            <span>Metal: 14K Gold</span><br>
-            <span>Metal Color: Yellow/Rose/Green</span><br>
-            <span>Stones: Round Yellow Sapphire 0.23</span><br>
-            <span>Round Diamond 0.09</span>
+            <span>Chất liệu: Vàng Trắng 14K</span><br>
+            <span>Đá: Kim Cương Tròn 1.5 carat</span><br>
+            <span>Chuyển động: Quartz</span><br>
+            <span>Có hả năng chống nước</span>
         `,
         images: [
-            "assets/User/img/watch/1.png",
-            "assets/User/img/watch/2.png",
-            "assets/User/img/watch/3.png",
-            "assets/User/img/watch/4.png"
+            "assets/User/img/watch/2-1.jpg",
+            "assets/User/img/watch/2-2.jpg",
+            "assets/User/img/watch/2-3.jpg",
+            "assets/User/img/watch/2-4.jpg"
         ]
     },
     {
         id: 3,
-        name: "Vòng tay(Hàng tặng)",
-        category: "bracelet",
-        price: 0,
+        name: "Đồng Hồ Bạch Kim",
+        category: "watch",
+        price: 114000000,
         description: `
-            <span>Metal: 14K Gold</span><br>
-            <span>Metal Color: White</span><br>
-            <span>Emerald Cut Blue Sapphire 1.52</span><br>
-            <span>Natural Diffused Ceylon Sapphire</span>
+            <span>Chất liệu: Bạch Kim</span><br>
+            <span>Chuyển động: Cơ học</span><br>
+            <span>Chức năng: Chức năng bấm giờ, Ngày</span><br>
+            <span>Có hả năng chống nước</span>
         `,
         images: [
-            "assets/User/img/bracelet/1.png",
-            "assets/User/img/bracelet/2.png",
-            "assets/User/img/bracelet/3.png",
-            "assets/User/img/bracelet/4.png"
+            "assets/User/img/watch/3-1.jpg",
+            "assets/User/img/watch/3-2.jpg",
+            "assets/User/img/watch/3-3.jpg",
+            "assets/User/img/watch/3-4.jpg"
         ]
     },
     {
         id: 4,
-        name: "White Gold Diamond Palm Tree Necklace",
-        category: "necklace",
-        price: 460000,
+        name: "Đồng Hồ Vàng Hồng Sang Trọng",
+        category: "watch",
+        price: 80750000,
         description: `
-            <span>Metal: 14K Gold</span><br>
-            <span>Metal Color: White</span><br>
-            <span>Stones: Round Diamond 9.19</span><br>
-            <span>Length: 7" Width:1/2"</span>
+            <span>Chất liệu: Vàng Hồng 18K</span><br>
+            <span>Chuyển động: Tự động</span><br>
+            <span>Đá: Kim Cương Accent</span><br>
+            <span>Có hả năng chống nước</span>
         `,
         images: [
-            "assets/User/img/necklace/1.png",
-            "assets/User/img/necklace/2.png",
-            "assets/User/img/necklace/3.png",
-            "assets/User/img/necklace/4.png"
+            "assets/User/img/watch/4-1.jpg",
+            "assets/User/img/watch/4-2.jpg",
+            "assets/User/img/watch/4-3.jpg",
+            "assets/User/img/watch/4-4.jpg"
         ]
     },
     {
         id: 5,
-        name: "White Sapphire Band Ring",
-        category: "ring",
-        price: 760000,
+        name: "Đồng Hồ Mặt Sapphire",
+        category: "watch",
+        price: 93100000,
         description: `
-            <span>Metal: 14K Gold</span><br>
-            <span>Metal Color: Rose/Yellow/White</span><br>
-            <span>Round Ruby 0.99</span><br>
-            <span>Oval Emerald 1.02</span>
+            <span>Chất liệu: Vàng 14K Vàng Vàng</span><br>
+            <span>Đá: Mặt Sapphire Xanh</span><br>
+            <span>Chuyển động: Cơ học Thụy Sĩ</span><br>
+            <span>Khong chống nước</span>
         `,
         images: [
-            "assets/User/img/ring/1.png",
-            "assets/User/img/ring/2.png",
-            "assets/User/img/ring/3.png",
-            "assets/User/img/ring/4.png"
+            "assets/User/img/watch/5-1.jpg",
+            "assets/User/img/watch/5-2.jpg",
+            "assets/User/img/watch/5-3.jpg",
+            "assets/User/img/watch/5-4.jpg"
         ]
     },
     {
         id: 6,
-        name: "Vòng cổ(Hàng tặng)",
-        category: "necklace",
-        price: 0,
+        name: "Nhẫn Đính Hôn Kim Cương",
+        category: "ring",
+        price: 142500000,
         description: `
-            <span>Metal: 14K Gold</span><br>
-            <span>Metal Color: Rose/Yellow/White</span><br>
-            <span>Round Emerald 0.99</span><br>
-            <span>Oval Emerald 1.02</span>
+            <span>Chất liệu: Bạch Kim</span><br>
+            <span>Đá: Kim Cương Tròn 2 Carat</span><br>
+            <span>Size Nhẫn: 6</span><br>
+            <span>Kiểu Dáng: Đặt Chân</span>
         `,
         images: [
-            "assets/User/img/necklace/1.png",
-            "assets/User/img/necklace/2.png",
-            "assets/User/img/necklace/3.png",
-            "assets/User/img/necklace/4.png"
+            "assets/User/img/ring/1-1.jpg",
+            "assets/User/img/ring/1-2.jpg",
+            "assets/User/img/ring/1-3.jpg",
+            "assets/User/img/ring/1-4.jpg"
         ]
     },
     {
         id: 7,
-        name: "Diamond Ring",
+        name: "Nhẫn Vàng và Ruby",
         category: "ring",
-        price: 1400000,
+        price: 80750000,
         description: `
-            <span>Metal: 14K Gold</span><br>
-            <span>Metal Color: Rose/Yellow/White</span><br>
-            <span>Round Ruby 0.99</span><br>
-            <span>Oval Emerald 1.02</span>
+            <span>Chất liệu: Vàng 18K</span><br>
+            <span>Đá: Ruby Tròn 1.5 Carat</span><br>
+            <span>Size Nhẫn: 7</span><br>
+            <span>Kiểu Dáng: Bezel</span>
         `,
         images: [
-            "assets/User/img/ring/1.png",
-            "assets/User/img/ring/2.png",
-            "assets/User/img/ring/3.png",
-            "assets/User/img/ring/4.png"
+            "assets/User/img/ring/2-1.jpg",
+            "assets/User/img/ring/2-2.jpg",
+            "assets/User/img/ring/2-3.jpg",
+            "assets/User/img/ring/2-4.jpg"
         ]
     },
     {
         id: 8,
-        name: "Vòng cổ(Hàng tặng)",
-        category: "necklace",
-        price: 0,
+        name: "Nhẫn Solitaire Ngọc Lục Bảo",
+        category: "ring",
+        price: 90250000,
         description: `
-            <span>Metal: 14K Gold</span><br>
-            <span>Metal Color: Rose/Yellow/White</span><br>
-            <span>Round Emerald 0.99</span><br>
-            <span>Oval Emerald 1.02</span>
+            <span>Chất liệu: Vàng Trắng 18K</span><br>
+            <span>Đá: Ngọc Lục Bảo 2 Carat</span><br>
+            <span>Size Nhẫn: 6.5</span><br>
+            <span>Kiểu Dáng: Đặt Chân</span>
         `,
         images: [
-            "assets/User/img/necklace/1.png",
-            "assets/User/img/necklace/2.png",
-            "assets/User/img/necklace/3.png",
-            "assets/User/img/necklace/4.png"
+            "assets/User/img/ring/3-1.jpg",
+            "assets/User/img/ring/3-2.jpg",
+            "assets/User/img/ring/3-3.jpg",
+            "assets/User/img/ring/3.jpg"
         ]
     },
     {
         id: 9,
-        name: "Đồng hồ(Hàng tặng)",
-        category: "watch",
-        price: 0,
+        name: "Nhẫn Ngọc Lục Bảo và Kim Cương",
+        category: "ring",
+        price: 104500000,
         description: `
-            <span>Metal: 14K Gold</span><br>
-            <span>Metal Color: Yellow/Rose/Green</span><br>
-            <span>Stones: Round Yellow Sapphire 0.23</span><br>
-            <span>Round Diamond 0.09</span>
+            <span>Chất liệu: Vàng Trắng 14K</span><br>
+            <span>Đá: Ngọc Lục Bảo 1.2 Carat, Kim Cương 0.5 Carat</span><br>
+            <span>Size Nhẫn: 8</span><br>
+            <span>Kiểu Dáng: Kênh</span>
         `,
         images: [
-            "assets/User/img/watch/1.png",
-            "assets/User/img/watch/2.png",
-            "assets/User/img/watch/3.png",
-            "assets/User/img/watch/4.png"
+            "assets/User/img/ring/4-1.jpg",
+            "assets/User/img/ring/4-2.jpg",
+            "assets/User/img/ring/4-3.jpg",
+            "assets/User/img/ring/4-4.jpg"
         ]
     },
-
-
- {
+    {
         id: 10,
-        name: "Gold Hoop Earrings",
-        category: "earrings",
-        price: 350000,
-         description: `
-           <span>Metal: 14K Gold</span><br>
-           <span>Diameter: 25mm</span><br>
-           <span>Style: Hoop</span><br>
-           <span>Finish: High Polish</span>
-       `,
+        name: "Bộ Nhẫn Cưới Bạch Kim",
+        category: "ring",
+        price: 161500000,
+        description: `
+            <span>Chất liệu: Bạch Kim</span><br>
+            <span>Đá: Kim Cương Tròn 1 Carat</span><br>
+            <span>Bộ Sản Phẩm: Nhẫn Đính Hôn và Nhẫn Cưới</span><br>
+            <span>Kiểu Dáng: Đặt Chân</span>
+        `,
         images: [
-            "assets/User/img/earrings/7-1.jpg",
-            "assets/User/img/earrings/7-2.jpg",
-            "assets/User/img/earrings/7-3.jpg",
-            "assets/User/img/earrings/7-4.jpg"
+            "assets/User/img/ring/5-1.jpg",
+            "assets/User/img/ring/5-2.jpg",
+            "assets/User/img/ring/5-3.jpg",
+            "assets/User/img/ring/5-4.jpg"
         ]
     },
     {
@@ -373,6 +371,95 @@ const Product = [
             "assets/User/img/bracelet/20-4.jpg"
         ]
     },
-
+    {
+        id: 21,
+        name: "Bông Tai 2 Trái Tim Vàng Hồng",
+        category: "earring",
+        price: 4000000,
+        description: `
+            <span>Black Month | Sale off 10~20%</span><br>
+            <span>Metal Color: Rose</span><br>
+            <span>Emerald Cut Blue Sapphire 1.52</span><br>
+            <span>Natural Diffused Ceylon Sapphire</span>
+        `,
+        images: [
+            "assets/User/img/earrings/21-1.jpg",
+            "assets/User/img/earrings/21-2.jpg",
+            "assets/User/img/earrings/21-3.jpg",
+            "assets/User/img/earrings/21-4.jpg"
+        ]
+    },
+    {
+        id: 22,
+        name: "Bông Tai Bạc Bông Hoa",
+        category: "earring",
+        price: 3000000,
+        description: `
+            <span>Black Month | Sale off 10~20%</span><br>
+            <span>Metal Color: Rose</span><br>
+            <span>Emerald Cut Blue Sapphire 1.52</span><br>
+            <span>Natural Diffused Ceylon Sapphire</span>
+        `,
+        images: [
+            "assets/User/img/earrings/22-1.jpg",
+            "assets/User/img/earrings/22-2.jpg",
+            "assets/User/img/earrings/22-3.jpg",
+            "assets/User/img/earrings/22-4.jpg"
+        ]
+    },
+    {
+        id: 23,
+        name: "Bông Tai Bạc Đính Akoya",
+        category: "earring",
+        price: 3500000,
+        description: `
+            <span>Black Month | Sale off 10~20%</span><br>
+            <span>Metal Color: Rose</span><br>
+            <span>Emerald Cut Blue Sapphire 1.52</span><br>
+            <span>Natural Diffused Ceylon Sapphire</span>
+        `,
+        images: [
+            "assets/User/img/earrings/23-1.jpg",
+            "assets/User/img/earrings/23-2.jpg",
+            "assets/User/img/earrings/23-3.jpg",
+            "assets/User/img/earrings/23-4.jpg"
+        ]
+    },
+    {
+        id: 24,
+        name: "Bông Tai Bạc Hoa Cánh Bướm",
+        category: "earring",
+        price: 10000000,
+        description: `
+            <span>Black Month | Sale off 10~20%</span><br>
+            <span>Metal Color: Rose</span><br>
+            <span>Emerald Cut Blue Sapphire 1.52</span><br>
+            <span>Natural Diffused Ceylon Sapphire</span>
+        `,
+        images: [
+            "assets/User/img/earrings/24-1.jpg",
+            "assets/User/img/earrings/24-2.jpg",
+            "assets/User/img/earrings/24-3.jpg",
+            "assets/User/img/earrings/24-4.jpg"
+        ]
+    },
+    {
+        id: 25,
+        name: "Bông Tai Bac Mặt Trăng Khuyết",
+        category: "earring",
+        price: 12000000,
+        description: `
+            <span>Black Month | Sale off 10~20%</span><br>
+            <span>Metal Color: Rose</span><br>
+            <span>Emerald Cut Blue Sapphire 1.52</span><br>
+            <span>Natural Diffused Ceylon Sapphire</span>
+        `,
+        images: [
+            "assets/User/img/earrings/25-1.jpg",
+            "assets/User/img/earrings/25-2.jpg",
+            "assets/User/img/earrings/25-3.jpg",
+            "assets/User/img/earrings/25-4.jpg"
+        ]
+    },
 ];
 
