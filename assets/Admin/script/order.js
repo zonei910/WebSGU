aa05287a9a5146965f36dcae2fb89d209693809d
zonei <haom242@gmail.com>
2024-11-27 21:20:08 +0700

    làm tiếp

--- assets/Admin/script/order.js
+++ assets/Admin/script/order.js
@@ -12,6 +12,34 @@ function filter_order(){
 		time.classList.add("hide");
 		let timebtn = document.querySelector("#Display .order .filterbtn");
 		timebtn.classList.add("hide");
+	}
+}
+
+function showDonHang(){
+	let donhang = JSON.parse(localStorage.getItem("donhang"));
+	if(donhang.length == 0){
+		alert("Chưa có đơn hàng nào");
+	}else{
+		let sosanphammoitrang = 3;
+		let sotranghientai = 1;
+		let tongsotrang = Math.ceil(donhang.length/sosanphammoitrang);
+		let vitrihientai = (sotranghientai - 1) * sosanphammoitrang;
+		let btn = '';
+		for(let i = 1 ; i<=tongsotrang ; i ++){
+			btn += `
+				<div class="item" onclick="nuttrang_order(${i})">${i}</div>	
+
+			`
+		}
+
+
+
+
+
+
+
+		let showbtn = document.querySelector("#Display .order .foot .pagination");
+		showbtn.innerHTML = btn;
 
 
 
@@ -20,3 +48,5 @@ function filter_order(){
 
 	}
 }
+
+showDonHang();
