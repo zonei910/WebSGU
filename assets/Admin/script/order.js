6d6665d687b5f8dc5779d260de96990a60126d5a
zonei <haom242@gmail.com>
2024-11-27 22:27:33 +0700

    hehe

--- assets/Admin/script/order.js
+++ assets/Admin/script/order.js
@@ -17,7 +17,7 @@ function filter_order(){
 
 function showDonHang(){
 	let donhang = JSON.parse(localStorage.getItem("donhang"));
-	if(donhang.length == 0){
+	if(donhang == null){
 		alert("Chưa có đơn hàng nào");
 	}else{
 		let sosanphammoitrang = 3;
@@ -25,28 +25,200 @@ function showDonHang(){
 		let tongsotrang = Math.ceil(donhang.length/sosanphammoitrang);
 		let vitrihientai = (sotranghientai - 1) * sosanphammoitrang;
 		let btn = '';
+		if(tongsotrang != 1){
 		for(let i = 1 ; i<=tongsotrang ; i ++){
 			btn += `
 				<div class="item" onclick="nuttrang_order(${i})">${i}</div>	
 
 			`
 		}
-
-
-
-
-
+	}
+		let a ='';
+		let n = 0;
+		for(let i = vitrihientai ; i<donhang.length;i++){
+			let trangthai;
+			if(donhang[i].status == 0){
+				trangthai = "Chưa xử lý";
+			}else if(donhang[i].status == 1){
+				trangthai = "Đã duyệt";
+			}else if(donhang[i].status == 2){
+				trangthai = "Đã giao hàng thành công";
+			}
+			n++;
+			a += `
+			<tr>
+				  <td>${donhang[i].id}</td>
+                            <td>${donhang[i].tenKH}</td>
+                            <td>${donhang[i].tongtien}</td>
+                            <td>${donhang[i].ngaydat}</td>
+                            <td>${donhang[i].ngayduyet == 0?"Chưa duyệt": donhang[i].ngayduyet}</td>
+                            <td>${trangthai}</td>
+                            <td class="button">
+                                <div name="${donhang[i].id}" onclick="xulydonhang(this)">Xử lý đơn hàng</div>
+                            </td>
+                    </tr>   
+			
+			`;
+			if(n == sosanphammoitrang) break;
+		}
 
 
 		let showbtn = document.querySelector("#Display .order .foot .pagination");
 		showbtn.innerHTML = btn;
-
-
-
-
-
+		let showdon = document.querySelector("#Display .order .main table tbody");
+		showdon.innerHTML = a;
 
 	}
 }
 
-showDonHang();
+function nuttrang_order(a){
+	let donhang = JSON.parse(localStorage.getItem("donhang"));
+	let sosanphammoitrang = 3;
+	let vitrihientai = (a - 1) * sosanphammoitrang;
+
+	let n = 0;
+	let b = ``;
+	for(let i = vitrihientai ; i < donhang.length ; i ++){
+		let trangthai;
+			if(donhang[i].status == 0){
+				trangthai = "Chưa xử lý";
+			}else if(donhang[i].status == 1){
+				trangthai = "Đã duyệt";
+			}else if(donhang[i].status == 2){
+				trangthai = "Đã giao hàng thành công";
+			}
+			n++;
+			b += `
+			<tr>
+				  <td>${donhang[i].id}</td>
+                            <td>${donhang[i].tenKH}</td>
+                            <td>${donhang[i].tongtien}</td>
+                            <td>${donhang[i].ngaydat}</td>
+                            <td>${donhang[i].ngayduyet == 0?"Chưa duyệt": donhang[i].ngayduyet}</td>
+                            <td>${trangthai}</td>
+                            <td class="button">
+                                <div name="${donhang[i].id}" onclick="xulydonhang(this)">Xử lý đơn hàng</div>
+                            </td>
+                    </tr>   
+			
+			`;
+			if(n == sosanphammoitrang) break;
+	} 
+
+	let showdon = document.querySelector("#Display .order .main table tbody");
+		showdon.innerHTML = b;
+
+}
+
+let filterarr = [];
+
+function filter_order(){
+	filterarr = [];
+	let donhang = JSON.parse(localStorage.getItem("donhang"));
+	let select = document.querySelector("#Display .order .loc");
+	if(donhang == null){
+		alert("Chưa có đơn hàng nào");
+	}else{
+		if(select.value == "none"){
+			showDonHang();
+			return 0;
+		}
+		let sosanphammoitrang = 3;
+		let sotranghientai = 1;
+		let tongsotrang = Math.ceil(donhang.length/sosanphammoitrang);
+		let vitrihientai = (sotranghientai - 1) * sosanphammoitrang;
+		let btn = '';
+		if(tongsotrang != 1){
+		for(let i = 1 ; i<=tongsotrang ; i ++){
+			btn += `
+				<div class="item" onclick="nuttrangfilter_order(${i})">${i}</div>	
+
+			`
+		}
+	}	
+
+
+		for(let i = 0 ; i < donhang.length ; i++){
+			if(select.value == donhang[i].status){
+				filterarr.push(donhang[i]);
+			}
+		}
+
+		let a ='';
+		let n = 0;
+		for(let i = vitrihientai ; i<filterarr.length;i++){
+			let trangthai;
+			if(filterarr[i].status == 0){
+				trangthai = "Chưa xử lý";
+			}else if(filterarr[i].status == 1){
+				trangthai = "Đã duyệt";
+			}else if(filterarr[i].status == 2){
+				trangthai = "Đã giao hàng thành công";
+			}
+			n++;
+			a += `
+			<tr>
+				  <td>${filterarr[i].id}</td>
+                            <td>${filterarr[i].tenKH}</td>
+                            <td>${filterarr[i].tongtien}</td>
+                            <td>${filterarr[i].ngaydat}</td>
+                            <td>${filterarr[i].ngayduyet == 0?"Chưa duyệt": filterarr[i].ngayduyet}</td>
+                            <td>${trangthai}</td>
+                            <td class="button">
+                                <div name="${filterarr[i].id}" onclick="xulydonhang(this)">Xử lý đơn hàng</div>
+                            </td>
+                    </tr>   
+			
+			`;
+			if(n == sosanphammoitrang) break;
+		}
+
+
+		let showbtn = document.querySelector("#Display .order .foot .pagination");
+		showbtn.innerHTML = btn;
+		let showdon = document.querySelector("#Display .order .main table tbody");
+		showdon.innerHTML = a;
+		console.log(filterarr);
+	}
+}
+
+
+function nuttrangfilter_order(a){
+	let sosanphammoitrang = 3;
+	let vitrihientai = (a - 1) * sosanphammoitrang;
+
+	let n = 0;
+	let b = ``;
+	for(let i = vitrihientai ; i < filterarr.length ; i ++){
+		let trangthai;
+			if(filterarr[i].status == 0){
+				trangthai = "Chưa xử lý";
+			}else if(filterarr[i].status == 1){
+				trangthai = "Đã duyệt";
+			}else if(filterarr[i].status == 2){
+				trangthai = "Đã giao hàng thành công";
+			}
+			n++;
+			b += `
+			<tr>
+				  <td>${filterarr[i].id}</td>
+                            <td>${filterarr[i].tenKH}</td>
+                            <td>${filterarr[i].tongtien}</td>
+                            <td>${filterarr[i].ngaydat}</td>
+                            <td>${filterarr[i].ngayduyet == 0?"Chưa duyệt": filterarr[i].ngayduyet}</td>
+                            <td>${trangthai}</td>
+                            <td class="button">
+                                <div name="${filterarr[i].id}" onclick="xulydonhang(this)">Xử lý đơn hàng</div>
+                            </td>
+                    </tr>   
+			
+			`;
+			if(n == sosanphammoitrang) break;
+}
+let showdon = document.querySelector("#Display .order .main table tbody");
+showdon.innerHTML = b;
+}
+
+function find_order(){
+	
+}
