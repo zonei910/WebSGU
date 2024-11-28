1c446b722e660c2dd49c7a6225f216ca63bbabd9
zonei <haom242@gmail.com>
2024-11-28 21:16:42 +0700

    Xong đơn hàng

--- assets/Admin/script/order.js
+++ assets/Admin/script/order.js
@@ -1,26 +1,11 @@
-function filter_order(){
-	let select = document.querySelector("#Display .order .loc");
-	if(select.value == "time"){
-		let time = document.querySelector("#Display .order .time");
-		time.classList.remove("hide");
-		let timebtn = document.querySelector("#Display .order .filterbtn");
-		timebtn.classList.remove("hide");
-		console.log("Hello");
-	}
-	else{
-		let time = document.querySelector("#Display .order .time");
-		time.classList.add("hide");
-		let timebtn = document.querySelector("#Display .order .filterbtn");
-		timebtn.classList.add("hide");
-	}
-}
+
 
 function showDonHang(){
 	let donhang = JSON.parse(localStorage.getItem("donhang"));
 	if(donhang == null){
 		alert("Chưa có đơn hàng nào");
 	}else{
-		let sosanphammoitrang = 3;
+		let sosanphammoitrang = 8;
 		let sotranghientai = 1;
 		let tongsotrang = Math.ceil(donhang.length/sosanphammoitrang);
 		let vitrihientai = (sotranghientai - 1) * sosanphammoitrang;
@@ -73,7 +58,7 @@ function showDonHang(){
 
 function nuttrang_order(a){
 	let donhang = JSON.parse(localStorage.getItem("donhang"));
-	let sosanphammoitrang = 3;
+	let sosanphammoitrang = 8;
 	let vitrihientai = (a - 1) * sosanphammoitrang;
 
 	let n = 0;
@@ -116,6 +101,20 @@ function filter_order(){
 	filterarr = [];
 	let donhang = JSON.parse(localStorage.getItem("donhang"));
 	let select = document.querySelector("#Display .order .loc");
+	if(select.value == "time"){
+		let time = document.querySelector("#Display .order .time");
+		time.classList.remove("hide");
+		let timebtn = document.querySelector("#Display .order .filterbtn");
+		timebtn.classList.remove("hide");
+		return 0;
+	}
+	else{
+		let time = document.querySelector("#Display .order .time");
+		time.classList.add("hide");
+		let timebtn = document.querySelector("#Display .order .filterbtn");
+		timebtn.classList.add("hide");
+	}
+
 	if(donhang == null){
 		alert("Chưa có đơn hàng nào");
 	}else{
@@ -129,7 +128,7 @@ function filter_order(){
 				filterarr.push(donhang[i]);
 			}
 		}
-		let sosanphammoitrang = 3;
+		let sosanphammoitrang = 8;
 		let sotranghientai = 1;
 		let tongsotrang = Math.ceil(filterarr.length/sosanphammoitrang);
 		let vitrihientai = (sotranghientai - 1) * sosanphammoitrang;
@@ -186,7 +185,7 @@ function filter_order(){
 
 
 function nuttrangfilter_order(a){
-	let sosanphammoitrang = 3;
+	let sosanphammoitrang = 8;
 	let vitrihientai = (a - 1) * sosanphammoitrang;
 
 	let n = 0;
@@ -262,7 +261,7 @@ function find_order(){
 	}
 	filterarr = findarr;
 
-	let sosanphammoitrang = 3;
+	let sosanphammoitrang = 8;
 	let sotranghientai = 1;
 	let tongsotrang = Math.ceil(filterarr.length/sosanphammoitrang);
 	let vitrihientai = (sotranghientai - 1) * sosanphammoitrang;
@@ -372,9 +371,180 @@ function xulydonhang(a){
 	`;
 	
 	right.innerHTML = `
-			
+			  <div class="right">
+                                <p>Tên: <span>${donhientai.tenKH}</span></p>
+                                <p>Số điện thoại: <span>${donhientai.phoneKH}</span></p>
+                                <p>Email: <span>${donhientai.emailKH}</span></p>
+                                <p style="width: max-content">Địa chỉ: <span>${donhientai.diachiKH}</span></p>
+                                <p>Giới tính: <span>${donhientai.genderKH == 'nu'?'Nữ':'Nam'}</span></p>
+                            </div>
 	
-	`
+	`;
+	
+	let watch = [];
+	let watchsum = 0;
+	for(let i = 0 ; i< donhientai.giohang.length ; i ++){
+		if(donhientai.giohang[i].loai == "watch"){
+			watch.push(donhientai.giohang[i]);
+			watchsum += (donhientai.giohang[i].gia * donhientai.giohang[i].soLuong);
+		}
+	}
+if(watch.length!=0){
+	let temp = ``;
+	for(let i = 0 ; i<watch.length ; i++){
+		temp += `
+				<tr>
+                                        <td><img src="${watch[i].hinh}" alt=""></td>
+                                        <td>${watch[i].id}</td>
+                                        <td>${watch[i].ten}</td>
+                                        <td>${watch[i].soLuong}</td>
+                                        <td>${watch[i].gia}</td>
+                                        <td>${watch[i].gia * watch[i].soLuong}</td>
+                                    </tr>
+		 `;
+	}
+
+	let showWatch = document.querySelector("#sp-giohang-watch");
+	showWatch.innerHTML = temp; 
+	let showWatchSum = document.querySelector("#Display .order .all .watch_sum");
+	showWatchSum.innerHTML = watchsum;
+}
 
 
+
+	let ring = [];
+	let ringsum = 0;
+
+for(let i = 0 ; i< donhientai.giohang.length ; i ++){
+		if(donhientai.giohang[i].loai == "ring"){
+			ring.push(donhientai.giohang[i]);
+			ringsum += (donhientai.giohang[i].gia * donhientai.giohang[i].soLuong);
+		}
+	}
+if(ring.length != 0){
+	 temp = ``;
+	for(let i = 0 ; i<ring.length ; i++){
+		temp += `
+				<tr>
+                                        <td><img src="${ring[i].hinh}" alt=""></td>
+                                        <td>${ring[i].id}</td>
+                                        <td>${ring[i].ten}</td>
+                                        <td>${ring[i].soLuong}</td>
+                                        <td>${ring[i].gia}</td>
+                                        <td>${ring[i].gia * ring[i].soLuong}</td>
+                                    </tr>
+		 `;
+	}
+
+	let showRing = document.querySelector("#sp-giohang-ring");
+	showRing.innerHTML = temp; 
+	let showRingSum = document.querySelector("#Display .order .all .ring_sum");
+	showRingSum.innerHTML = ringsum;
+}
+
+
+	let necklace = [];
+	let necklacesum = 0;
+
+for(let i = 0 ; i< donhientai.giohang.length ; i ++){
+		if(donhientai.giohang[i].loai == "necklace"){
+			necklace.push(donhientai.giohang[i]);
+			necklacesum += (donhientai.giohang[i].gia * donhientai.giohang[i].soLuong);
+		}
+	}
+if(necklace.length !=0){
+	 temp = ``;
+	for(let i = 0 ; i<necklace.length ; i++){
+		temp += `
+				<tr>
+                                        <td><img src="${necklace[i].hinh}" alt=""></td>
+                                        <td>${necklace[i].id}</td>
+                                        <td>${necklace[i].ten}</td>
+                                        <td>${necklace[i].soLuong}</td>
+                                        <td>${necklace[i].gia}</td>
+                                        <td>${necklace[i].gia * necklace[i].soLuong}</td>
+                                    </tr>
+		 `;
+	}
+
+	let showNecklace = document.querySelector("#sp-giohang-necklace");
+	showNecklace.innerHTML = temp; 
+	let showNecklaceSum = document.querySelector("#Display .order .all .necklace_sum");
+	showNecklaceSum.innerHTML = necklacesum;
+
+}
+
+
+	let bracelet = [];
+	let braceletsum = 0;
+
+for(let i = 0 ; i< donhientai.giohang.length ; i ++){
+		if(donhientai.giohang[i].loai == "bracelet"){
+			bracelet.push(donhientai.giohang[i]);
+			braceletsum += (donhientai.giohang[i].gia * donhientai.giohang[i].soLuong);
+		}
+	}
+if(bracelet.length !=0){
+	 temp = ``;
+	for(let i = 0 ; i<necklace.length ; i++){
+		temp += `
+				<tr>
+                                        <td><img src="${bracelet[i].hinh}" alt=""></td>
+                                        <td>${bracelet[i].id}</td>
+                                        <td>${bracelet[i].ten}</td>
+                                        <td>${bracelet[i].soLuong}</td>
+                                        <td>${bracelet[i].gia}</td>
+                                        <td>${bracelet[i].gia * bracelet[i].soLuong}</td>
+                                    </tr>
+		 `;
+	}
+
+	let showBracelet = document.querySelector("#sp-giohang-bracelet");
+	showBracelet.innerHTML = temp; 
+	let showBraceletSum = document.querySelector("#Display .order .all .bracelet_sum");
+	shownBraceletSum.innerHTML = braceletsum;
+
+}
+
+
+
+	let earring = [];
+	let earringsum = 0;
+	
+	for(let i = 0 ; i< donhientai.giohang.length ; i ++){
+		if(donhientai.giohang[i].loai == "earring"){
+		earring.push(donhientai.giohang[i]);
+		earringsum += (donhientai.giohang[i].gia * donhientai.giohang[i].soLuong);
+		}
+	}
+if(earring.length !=0){
+	 temp = ``;
+	for(let i = 0 ; i<earring.length ; i++){
+		temp += `
+				<tr>
+                                        <td><img src="${earring[i].hinh}" alt=""></td>
+                                        <td>${earring[i].id}</td>
+                                        <td>${earring[i].ten}</td>
+                                        <td>${earring[i].soLuong}</td>
+                                        <td>${earring[i].gia}</td>
+                                        <td>${earring[i].gia * earring[i].soLuong}</td>
+                                    </tr>
+		 `;
+	}
+
+	let showEarring = document.querySelector("#sp-giohang-earring");
+	showEarring.innerHTML = temp; 
+	let showEarringSum = document.querySelector("#Display .order .all .earring_sum");
+	showEarringSum.innerHTML = earringsum;
+
+}
+
+
+
+
+}
+
+
+function duyet(madon){
+	let donhang = JSON.parse(localStorage.getItem("donhang"));
 }
