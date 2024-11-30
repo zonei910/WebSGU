b36e5691ac881b354f9fcd11386e581077051f82
ChimUng <duynguyen19087@gmail.com>
2024-11-30 07:22:43 +0700

    sdt

--- assets/User/script/Login_Signin.js
+++ assets/User/script/Login_Signin.js
@@ -36,6 +36,12 @@ function dangky(event) {
     let confirmpassword = document.getElementById('register-confirm-password').value.trim();
     let quanne = document.getElementById('register-quan').value;
     console.log(quanne);
+
+    if (Number(phone).toString() !== phone) {
+        alert("Số điện thoại không hợp lệ! Số điện thoại chỉ được chứa các chữ số");
+        return false;
+    }
+
     // kiem tra nhap lai mat khau
     if ( password !== confirmpassword ){
         alert("Mật khẩu không khớp!");
